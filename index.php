36ba219d1faa6d1a43dbf09b650b8e2288959d62
murali123455 <137748569+murali123455@users.noreply.github.com>
2024-01-10 15:46:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello muralis!<br>";
-echo "I'm about to learn PHP!<br>";
+echo "I'm about to !<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?> 
 
